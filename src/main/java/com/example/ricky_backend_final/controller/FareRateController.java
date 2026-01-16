4e7fc4fc1cde55c1102249ef03f09b830874eab1
Jasjyot Singh <157724791+jjyo123456@users.noreply.github.com>
2026-01-17 00:07:13 +0530

    Update CORS configuration to allow all origins

--- src/main/java/com/example/ricky_backend_final/controller/FareRateController.java
+++ src/main/java/com/example/ricky_backend_final/controller/FareRateController.java
@@ -11,9 +11,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/fare")
-@CrossOrigin(origins = {"http://localhost:3000",
-            "http://localhost:5173",
-            "https://anveshan-x-ricky-ap.vercel.app"})
+@CrossOrigin(origins = {"*"})
 public class FareRateController {
 
     private final FareRateService fareRateService;
